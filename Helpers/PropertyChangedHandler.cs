696480c8d48aaff6278871032a108548b53e205c
Rickdeman <r.deman@joopvanzanten.nl>
2020-12-23 13:40:01 +0100

    New helper: PropertyChangedHandler

---
+++ Helpers/PropertyChangedHandler.cs
@@ -0,0 +1,51 @@
+﻿/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using System.ComponentModel;
+using System.Windows.Threading;
+
+namespace HicadCommunity.Helpers
+{
+	/// <summary>
+	/// PropertyChanged helper
+	/// </summary>
+	public class PropertyChangedHandler : INotifyPropertyChanged
+	{
+		/// <summary>
+		/// Get the Dispatcher of the objects ownwer
+		/// </summary>
+		protected readonly Dispatcher Dispatcher;
+
+		/// <summary>
+		/// Create new PropertyChagnedHandler
+		/// </summary>
+		public PropertyChangedHandler()
+		{
+			// Save the Dispatcher of the objects ownwer
+			Dispatcher = Dispatcher.CurrentDispatcher;
+		}
+
+		/// <summary>
+		/// Propertychanged event handler
+		/// </summary>
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		/// <summary>
+		/// Sync fields by variable name
+		/// </summary>
+		/// <param name="propertyName">Name of the variable</param>
+		protected void NotifyPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+}
