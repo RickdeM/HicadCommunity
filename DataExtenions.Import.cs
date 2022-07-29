ed971156a953ddbe668835c4d463f17b3d78af76
Rick de Man <r.deman@joopvanzanten.nl>
2022-07-29 13:03:43 +0200

    Splitting DataExtensions into multiple files

---
+++ DataExtenions.Import.cs
@@ -0,0 +1,217 @@
+﻿/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using HiCAD.Data;
+using ISD.CAD.Data;
+using ISD.CAD.IO;
+using ISD.Math;
+using System;
+using System.IO;
+
+namespace RDM.HicadCommunity
+{
+	public static partial class DataExtenions
+	{
+		#region STP
+
+		/// <summary>
+		/// Import a STP/STEP file into the provided scene
+		/// </summary>
+		/// <param name="scene">The scene where the object needs to imported in</param>
+		/// <param name="ms">The file which is stored in a MemoryStream</param>
+		/// <returns></returns>
+		public static Node ImportStp(this Scene scene, MemoryStream ms)
+		{
+			// Create a Filename
+			string tmpFile = Path.GetTempFileName();
+			try
+			{
+				// Create a filestream
+				using (FileStream fs = new FileStream(tmpFile, FileMode.Create, FileAccess.Write))
+				{
+					// Write to the file
+					ms.WriteTo(fs);
+				}
+				// Import the DXF/DWG
+				Node result = scene.ImportStp(tmpFile);
+				// return the result
+				return result;
+			} catch (Exception)
+			{
+				return default;
+			} finally
+			{
+				// Check if the file exists
+				if (File.Exists(tmpFile))
+					// Delete the temporary file
+					File.Delete(tmpFile);
+			}
+		}
+
+		/// <summary>
+		/// Import a STP/STEP file into the provided scene
+		/// </summary>
+		/// <param name="scene">The scane where the object needs to imported in</param>
+		/// <param name="file">The file which needs to be imported in the scene</param>
+		/// <returns></returns>
+		public static Node ImportStp(this Scene scene, FileInfo file) => scene.ImportStp(file.FullName);
+
+		/// <summary>
+		/// Import a STP/STEP file into the provided scene
+		/// </summary>
+		/// <param name="scene">The scane where the object needs to imported in</param>
+		/// <param name="file">The file which needs to be imported in the scene</param>
+		/// <returns></returns>
+		public static Node ImportStp(this Scene scene, string file)
+		{
+			// Note:
+			// STP import settings can only be managed in the CFGDB: Interfaces > General 3-D interfaces
+			// "AutoOptimise" and "Repair Tool" are disabled by default, its advised to enable these options
+			// See the online help for detailed information for both options
+
+			// Check if the parameters are correctly provided
+			if (scene is null)
+				throw new ArgumentNullException(nameof(scene));
+			if (file is null)
+				throw new ArgumentNullException(nameof(file));
+
+			try
+			{
+				// The scene must be active in order to load any new objects
+				if (!scene.Active)
+					throw new Exception("Provided Scene is not activated");
+				// Load the STP File
+				Node result = FileIO.Load(file, new StepImportSettings()) as Node;
+				// Return the imported object
+				return result;
+			} catch (Exception ex)
+			{
+				// Log the error for debugging purposes
+				Logger.Log(ex);
+				// Return the default value
+				return default;
+			}
+		}
+
+		#endregion STP
+
+		#region DXF
+
+		/// <summary>
+		/// Import a DXF from a byte
+		/// </summary>
+		/// <param name="scene">The scene where the object needs to imported in</param>
+		/// <param name="ms">The file which is stored in a MemoryStream</param>
+		/// <param name="autoMoveToZeroPoint">Automatically move the imported figure from BottomLeft to 0,0</param>
+		/// <param name="setScaleIndependent">Make the figure scale independent</param>
+		/// <param name="configurationFile">Import Configuration file, default is 'C:\HiCAD\sys\acadhcad.dat'</param>
+		/// <returns></returns>
+		public static Figure ImportDxfDwg(this Scene scene, MemoryStream ms, bool autoMoveToZeroPoint = false, bool setScaleIndependent = false, string configurationFile = null)
+		{
+			// Create a Filename
+			string tmpFile = Path.GetTempFileName();
+			try
+			{
+				// Create a filestream
+				using (FileStream fs = new FileStream(tmpFile, FileMode.Create, FileAccess.Write))
+				{
+					// Write to the file
+					ms.WriteTo(fs);
+				}
+				// Import the DXF/DWG
+				Figure result = scene.ImportDxfDwg(tmpFile, autoMoveToZeroPoint, setScaleIndependent, configurationFile);
+				// return the result
+				return result;
+			} catch (Exception)
+			{
+				return default;
+			} finally
+			{
+				// Check if the file exists
+				if (File.Exists(tmpFile))
+					// Delete the temporary file
+					File.Delete(tmpFile);
+			}
+		}
+
+		/// <summary>
+		/// Import a DXF/DWG file into the provided scene
+		/// </summary>
+		/// <param name="scene">The scane where the object needs to imported in</param>
+		/// <param name="file">The file which needs to be imported in the scene</param>
+		/// <param name="autoMoveToZeroPoint">Automatically move the imported figure from BottomLeft to 0,0</param>
+		/// <param name="setScaleIndependent">Make the figure scale independent</param>
+		/// <param name="configurationFile">Import Configuration file, default is 'C:\HiCAD\sys\acadhcad.dat'</param>
+		/// <returns></returns>
+		public static Figure ImportDxfDwg(this Scene scene, FileInfo file, bool autoMoveToZeroPoint = false, bool setScaleIndependent = false, string configurationFile = null)
+			=> scene.ImportDxfDwg(file.FullName, autoMoveToZeroPoint, setScaleIndependent, configurationFile);
+
+		/// <summary>
+		/// Import a DXF/DWG file into the provided scene
+		/// </summary>
+		/// <param name="scene">The scene where the object needs to imported in</param>
+		/// <param name="file">The file which needs to be imported in the scene</param>
+		/// <param name="autoMoveToZeroPoint">Automatically move the imported figure from BottomLeft to 0,0</param>
+		/// <param name="setScaleIndependent">Make the figure scale independent</param>
+		/// <param name="configurationFile">Import Configuration file, default is 'C:\HiCAD\sys\acadhcad.dat'</param>
+		/// <returns></returns>
+		public static Figure ImportDxfDwg(this Scene scene, string file, bool autoMoveToZeroPoint = false, bool setScaleIndependent = false, string configurationFile = null)
+		{
+			// Note:
+			// DXF/DWG import settings can only be managed in the CFGDB: Interfaces > General 3-D interfaces
+
+			// Check if the parameters are correctly provided
+			if (scene is null)
+				throw new ArgumentNullException(nameof(scene));
+			if (file is null)
+				throw new ArgumentNullException(nameof(file));
+
+			try
+			{
+				// The scene must be active in order to load any new objects
+				if (!scene.Active)
+					throw new Exception("Provided Scene is not activated");
+				// Load the DXF/DWG File, All configurations can be found in the CFGDB:s
+				FigureImpl result = FileIO.Load(
+					file,
+					string.IsNullOrEmpty(configurationFile) ? new DXFImportSettings() : new DXFImportSettings() { ConfigurationFile = configurationFile }
+				) as FigureImpl;
+				result.DrawingSheet = Context.ActiveScene.ActiveDrawingSheet;
+				// Check if the Figure should me moved as close the the Zero point
+				if (autoMoveToZeroPoint)
+				{
+					// Only move the figure when there is a distance between them
+					if (result.BoundingRect.BottomLeft.GetDistance(new Point2D()) > 0)
+						// Move BottomLeft to the Zero point
+						result.Move(new Vector2D(result.BoundingRect.BottomLeft, new Point2D()));
+				}
+				// Check if scale independent should be enablead
+				if (setScaleIndependent)
+					// Make
+					result.SetScaleIndependent(new Vector2D());
+				// Return the imported object
+				return result;
+			} catch (Exception ex)
+			{
+				// Log the error for debugging purposes
+				Logger.Log(ex);
+				// Return the default value
+				return default;
+			}
+		}
+
+		#endregion DXF
+	}
+}
---
+++ DataExtenions.Transformations.cs
@@ -0,0 +1,153 @@
+﻿/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using ISD.BaseTypes;
+using ISD.CAD.Data;
+using ISD.Math;
+
+namespace RDM.HicadCommunity
+{
+	public static partial class DataExtenions
+	{
+		/// <summary>
+		/// Rotate a Node
+		/// </summary>
+		/// <param name="coor">Node to be rotated</param>
+		/// <param name="angle">Angle in degrees</param>
+		/// <param name="vector">Rotating vector (angle is CCW, rotates from X-axis to Y-axis), default axis is Z</param>
+		/// <param name="point">Rotating point, default Point is 0,0,0</param>
+		/// <returns></returns>
+		public static CoordinateSystem Rotate(this CoordinateSystem coor, Angle angle, NormVector3D? vector = null, Point3D? point = null)
+		{
+			var tfs = new Transformation();
+			tfs.SetRotation(point ?? new Point3D(), vector ?? NormVector3D.E3, angle);
+			coor.Transform(tfs);
+			return coor;
+		}
+
+		/// <summary>
+		/// Rotate a Node
+		/// </summary>
+		/// <param name="node">Node to be rotated</param>
+		/// <param name="angle">Angle in degrees</param>
+		/// <param name="vector">Rotating vector (angle is CCW, rotates from X-axis to Y-axis), default axis is Z</param>
+		/// <param name="point">Rotating point, default Point is 0,0,0</param>
+		/// <returns></returns>
+		public static T Rotate<T>(this T node, Angle angle, NormVector3D? vector = null, Point3D? point = null) where T : Node
+		{
+			// Create Transformation
+			var tfs = new Transformation();
+			tfs.SetRotation(point ?? new Point3D(), vector ?? NormVector3D.E3, angle);
+			// Transform the Node
+			tfs.Apply(node);
+			// Return the node
+			return node;
+		}
+
+		/// <summary>
+		/// Move a node given two points
+		/// </summary>
+		/// <param name="node">Node to be moved</param>
+		/// <param name="start">Start point</param>
+		/// <param name="end">End point</param>
+		/// <returns></returns>
+		public static T Move<T>(this T node, Point3D start, Point3D end) where T : Node => node.Move(new Vector3D(start, end));
+
+		/// <summary>
+		/// Move a node by the given vector
+		/// </summary>
+		/// <param name="node">Node to be moved</param>
+		/// <param name="vec">Vector used for transformation</param>
+		/// <returns></returns>
+		public static T Move<T>(this T node, Vector3D vec) where T : Node
+		{
+			// Move the Node
+			new Transformation(vec).Apply(node);
+			// Return the Node
+			return node;
+		}
+
+		/// <summary>
+		/// Move a Node using Workingplanes
+		/// </summary>
+		/// <param name="node">Node to be moved</param>
+		/// <param name="start">Start WorkingPlane for transformation</param>
+		/// <param name="end">End WorkingPlane for transformation</param>
+		/// <returns></returns>
+		public static T Move<T>(this T node, WorkingPlane start, WorkingPlane end) where T : Node => node.Move(start.CoordinateSystem, end.CoordinateSystem);
+
+		/// <summary>
+		/// Move a Node using Workingplanes
+		/// </summary>
+		/// <param name="node">Node to be moved</param>
+		/// <param name="start">Start CoordinateSystem for movement </param>
+		/// <param name="end">End CoordinateSystem for transformation</param>
+		/// <returns></returns>
+		public static T Move<T>(this T node, CoordinateSystem start, WorkingPlane end) where T : Node => node.Move(start, end.CoordinateSystem);
+
+		/// <summary>
+		/// Move a Node using Workingplanes
+		/// </summary>
+		/// <param name="node">Node to be moved</param>
+		/// <param name="start">Start CoordinateSystem for movement </param>
+		/// <param name="end">End CoordinateSystem for transformation</param>
+		/// <returns></returns>
+		public static T Move<T>(this T node, WorkingPlane start, CoordinateSystem end) where T : Node => node.Move(start.CoordinateSystem, end);
+
+		/// <summary>
+		/// Move a Node using CoordinationSystems
+		/// </summary>
+		/// <param name="node">Node to be moved</param>
+		/// <param name="start">Start CoordinateSystem for movement </param>
+		/// <param name="end">End CoordinateSystem for transformation</param>
+		/// <returns></returns>
+		public static T Move<T>(this T node, CoordinateSystem start, CoordinateSystem end) where T : Node
+		{
+			// Move the Node
+			start.BaseTransformation(end).Apply(node);
+			// Return the Node
+			return node;
+		}
+
+		/// <summary>
+		/// Move the CoordinateSystem
+		/// </summary>
+		/// <param name="coor">Coor to be moved</param>
+		/// <param name="vec">Vector for transformation</param>
+		/// <returns></returns>
+		public static CoordinateSystem Move(this CoordinateSystem coor, Vector3D vec)
+		{
+			coor.Move(new Transformation(vec));
+			return coor;
+		}
+
+		/// <summary>
+		/// Move the CoordinateSystem
+		/// </summary>
+		/// <param name="coor">Coor to be moved</param>
+		/// <param name="firstPoint">First point for Vector creation</param>
+		/// <param name="secondPoint">Second point for Vector creation</param>
+		/// <returns></returns>
+		public static CoordinateSystem Move(this CoordinateSystem coor, Point3D firstPoint, Point3D secondPoint) => coor.Move(new Vector3D(firstPoint, secondPoint));
+
+		/// <summary>
+		/// Move the CoordinateSystem
+		/// </summary>
+		/// <param name="coor">Coor to be moved</param>
+		/// <param name="endPoint">End point for Vector creation, start=0,0,0</param>
+		/// <returns></returns>
+		public static CoordinateSystem Move(this CoordinateSystem coor, Point3D endPoint) => coor.Move(new Vector3D(new Point3D(), endPoint));
+	}
+}
